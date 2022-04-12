1ee2a20612bebb6bc0ade1a8f047372ede07daf8
vakampen92 <dalonso@ceab.csic.es>
2022-04-12 20:47:40 +0200

    Master Equation DIFFUSION_BD_2D

--- Definition_Master_Equation/DIFFUSION_BD_2D/Model_Parameters_Master_Equation.c
+++ Definition_Master_Equation/DIFFUSION_BD_2D/Model_Parameters_Master_Equation.c
@@ -1,14 +1,11 @@
 #include <MODEL.h>
 
-#define FEEDING_EVENT_FACTOR 100
-
 void Model_Parameters_Master_Equation(Parameter_Table * Table,
 				      int * No_of_CONFIGURATIONAL_STATES,
 				      int * n_DIMENSION,
 				      int * n_x, int * n_y, int * n_z)
 {
-  double Time     = Table->T->Time_1 - Table->T->Time_0; 
-  double N        = Average_Number_of_Feeding_Events( Table, Time ); 
+  
   
   * n_DIMENSION = 2;
   * n_z         = 1;
@@ -18,22 +15,6 @@ void Model_Parameters_Master_Equation(Parameter_Table * Table,
   * No_of_CONFIGURATIONAL_STATES = Table->TOTAL_No_of_CONSUMERS * ( * n_x ) ;
 }
 
-double Average_Number_of_Feeding_Events( Parameter_Table * Table, double T )
-{
-  double n_T;
-  double n_R, m_0, K_R, Theta, Nu;
-
-  K_R   = (double)Table->K_R;
-  n_R   = (double)Table->TOTAL_No_of_RESOURCES; /* which determines resource density */
-  m_0   = (double)Table->TOTAL_No_of_CONSUMERS;
-
-  Theta = Table->Alpha_C_0 * n_R/K_R;  /* -H9  [Alpha_C_0] */
-  Nu    = Table->Nu_C_0;               /* -H10 [Nu_C_0]    */
-  
-  n_T = Theta * Nu/(Nu+Theta) * ( T + Theta/Nu/( Nu + Theta) * (1.0-exp(-(Theta+Nu)*T)) ) * m_0;
-  
-  return (n_T);
-}
 
 
 
--- Definition_Master_Equation/DIFFUSION_BD_2D/ODE_sys_MaterEquation.c
+++ Definition_Master_Equation/DIFFUSION_BD_2D/ODE_sys_MaterEquation.c
@@ -4,14 +4,24 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_odeiv.h>
 
+
+void i_to_nm_Map(Parameter_Table * Table,
+		 int i, int * n_i, int * m_i);
+
+void nm_to_i_Map(Parameter_Table * Table,
+		 int * i, int n, int m);
+
+double Sumando(int a_0, int m);
+
+
 int function_ME (double t, const double y[], double dydt[], void *params)
 {
-  int i, No_of_CONFIGURATIONAL_STATES, n, m, m_0;
+  int i, No_of_CONFIGURATIONAL_STATES, n, m, a_0;
   int n_R; 
   int Sp; 
-  double K_R, Theta, Nu; 
-  int    i_0, i_1, i_2;
-  double A_0, A_1, A_2;
+  double K_R, Theta, Nu, Chi, Eta; 
+  int    i_1, i_2, i_3, i_4;
+  double A_1, A_2, A_3, A_4, A_0; 
   
   Parameter_Table * Table = (Parameter_Table *)params;
 
@@ -21,45 +31,89 @@ int function_ME (double t, const double y[], double dydt[], void *params)
 
   K_R   = (double)Table->K_R;
   n_R   = Table->TOTAL_No_of_RESOURCES;   /* which determines resource density */
-  m_0   = Table->TOTAL_No_of_CONSUMERS;
+  a_0   = Table->TOTAL_No_of_CONSUMERS;
   
   No_of_CONFIGURATIONAL_STATES = Table->MEq->No_of_CONFIGURATIONAL_STATES;
 
   Theta = Table->Alpha_C_0 * (double)n_R/K_R;  /* -H9  [Alpha_C_0] */
   Nu    = Table->Nu_C_0;                       /* -H10 [Nu_C_0]    */
+  Chi   = Table->Chi_C_0;                      /* -H11             */                      
+  Eta   = Table->Eta_C_0;                      /* -H12             */
   
   for (i=0; i<No_of_CONFIGURATIONAL_STATES; i++) {
 
-    n = i/m_0;
-    m = i%m_0; 
-    
-    i_0 =(n-1)*m_0 + m+1;
-    i_1 = n   *m_0 + m-1;
-    i_2 = n   *m_0 + m;
+    i_to_nm_Map(Table, i, &n, &m);             /* n: A  (free predators)    */ 
+                                               /* m: RA (handling predators */
 
-    A_0 = Theta * (double)(m-1) * y[i_0];
-    A_1 = Nu * (double)(m_0 - m + 1) * y[i_1];
-    A_2 = ((Theta-Nu)*(double)m + Nu*(double)m_0) * y[i_2];  
+    nm_to_i_Map(Table, &i_1, n+1, m-1);
+    nm_to_i_Map(Table, &i_2, n-1, m+1);
+    nm_to_i_Map(Table, &i_3, n+1, m+1);
+    nm_to_i_Map(Table, &i_4, n-1, m-1);
+   
+    A_1= Theta * (double)(n+1) * y[i_1];
+    A_2= Nu *  (double)(m + 1) * y[i_2];
+    A_3= Chi * (double)(m + 1)/K_R * (double)n+1 * y[i_3];
+    A_4= 0.5*Eta*(double)(a_0-n-m+2) * y[i_4];
+    A_0= (Theta*(double)n+Nu*(double)m+Chi*(double)m/K_R*(double)n+0.5*Eta*(double)(a_0-n-m))*y[i];
     
     if( n == 0 ) { 
       if( m == 0 ) {
-	dydt[i] = -A_2;
+	dydt[i] = -A_0 + A_3;                    /* dP(n=0, m=0)/dt  */
       }
       else { 
-	dydt[i] = A_1 - A_2;
+	dydt[i] =  A_1 + A_3 - A_0;               /* dP(n=0, m)/dt    */
       }
     }
     else{
       if( m == 0 ) {
-	dydt[i] = A_0 - A_2;
+	dydt[i] = A_2 + A_3 - A_0;               /* dP(n, m=0)/dt    */
       }
       else {
-	dydt[i] = A_0 + A_1 - A_2;
+	dydt[i] = A_1 + A_2 + A_3 + A_4 - A_0;   /* dP(n, m)/dt      */
+	                                         /* n and m not zero */
       }
     }    
   }
   
   return GSL_SUCCESS;
+}
+
+void i_to_nm_Map(Parameter_Table * Table,
+		 int i, int * n_i, int * m_i)
+{
+  int m, a_0;
+  double S, a_0;
+
+  a_0 = Table->TOTAL_No_of_CONSUMERS;
+  
+  m = 0;
+  S = Sumando(a_0, 1);
+
+  while ( (i - S) >= 0.0 ) {
+    m++;
+
+    S = Suma_m(m+1);
+  }
+
+  * n_i = i - Sumando(a_0, m);
+  * m_i = m;
+}
+
+void nm_to_i_Map(Parameter_Table * Table,
+		 int * i, int n, int m)
+{
+  int a_0 = Table->TOTAL_No_of_CONSUMERS; 
+  
+  * i = n + Sumando(a_0, m); 
+}
+
+double Sumando(int a_0, int m)
+{
+  double R;
+
+  R = (double)m/2.0 * (2.0*(a_0 + 1.0) - m + 1.0); 
+
+  return(R); 
 }
 
 int jacobian_ME (double t, const double y[], double *dfdy, double dfdt[],
